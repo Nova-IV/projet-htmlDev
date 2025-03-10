363f53028814327ca97d16e9bcd839dd6d5eea35
Nova-IV <sofiane.zaidi@outlook>
2025-03-10 17:01:28 +0100

    js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("loaded ok !");
-    const lang = ["fr", 'html', true, 'aled'];
-    console.log(lang);
+    let elTitre;
+    elTitre = document.querySelector("title");
+    elTitre.innerText = "Développement WEB";
 });
+
+/* let age, ville, nombre, version, chiffre
+
+const user = 5.4
+age = 15
+ville = "Paris"
+nombre = null
+version = (1.55555555555555555555555555)
+
+console.log(parseFloat(version).toFixed(3).length)
+
+const tabLang = ["html", 1.5, true]
+console.log(typeof tabLang) */
